a2dc0505a088304263475274c0a7bccf29ac8daa
Stoyanoww <pstoyanov04@gmail.com>
2024-04-11 11:21:23 +0300

    Register saving data done

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const serverApp = require('./server/app');
+const serverApp = require('./Server/app');
 
 const app = express();
 app.use(express.static('public'));
--- public/Scripts/register.js
+++ public/Scripts/register.js
@@ -4,34 +4,44 @@ document.addEventListener('DOMContentLoaded', function() {
     registerForm.addEventListener('submit', async function(event) {
         event.preventDefault();
 
-        // Collect form data
-        const formData = {
-            id: 1,
-            email: document.getElementById('email').value,
-            name: document.getElementById('names').value,
-            password: document.getElementById('password').value
-        };
+        var email = document.getElementById('email').value;
+        var name = document.getElementById('names').value;
+        var password = document.getElementById('password').value;
+        var cpassword = document.getElementById('password-confirm').value;
+        var resultRegister = document.getElementById('result-register');
 
-        // Send data to server
-        try {
-            const response = await fetch('http://localhost:3001/api/users', {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
-                },
-                body: JSON.stringify(formData)
-            });
 
-            if (!response.ok) {
-                throw new Error('Failed to register user');
+        if(password === cpassword){
+            try {
+                const user ={
+                    email: email,
+                    name: name,
+                    password: password
+                }
+                
+                const response = await fetch('http://localhost:3001/api/users', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify(user)
+                });
+    
+                if (!response.ok) {
+                    throw new Error('Failed to register user');
+                }
+    
+                const data = await response.json();
+                console.log('User registered successfully:', data);
+                window.location.href = "login.html"
+            } catch (error) {
+                console.error('Error registering user:', error);
+                resultRegister.textContent = 'Failed to register user. Please try again later.';
             }
-
-            const data = await response.json();
-            console.log('User registered successfully:', data);
-        } catch (error) {
-            console.error('Error registering user:', error);
-            const resultRegister = document.getElementById('result-register');
-            resultRegister.textContent = 'Failed to register user. Please try again later.';
+        }else{
+            resultRegister.textContent = "Паролите Ви не съвпадат";
+            document.getElementById('password').value = '';
+        document.getElementById('password-confirm').value = '';
         }
     });
 });
