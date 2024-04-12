88e3e39fb0d9cf664ce5b6bca5cf415ef4060a34
Stoyanoww <pstoyanov04@gmail.com>
2024-04-13 01:58:16 +0300

    Services Logic Done

--- public/Scripts/admin.js
+++ public/Scripts/admin.js
@@ -47,14 +47,11 @@ async function displayAllServices() {
                 }
             });
             viewButton.addEventListener('click', () => {
-                // Fill form inputs with service values
                 document.getElementById('serviceName').value = service.name;
                 document.getElementById('serviceDescription').value = service.desc;
                 const imageUrl = service.imageUrl.replace('public', '..');
-                console.log(imageUrl);
                 document.getElementById('currentImage').src = imageUrl;
-
-                window.scrollTo(0, 0);
+                document.getElementById('serviceId').value = service.id;
             });
 
             itemContent.appendChild(serviceName);
@@ -73,6 +70,12 @@ document.getElementById('service-add').addEventListener('click', async (event) =
     event.preventDefault(); 
     
     const serviceName = document.getElementById('serviceName').value;
+    const existingService = await checkServiceExistence(serviceName);
+    if (existingService) {
+        document.getElementById('serviceName').value = "";
+      document.getElementById('result-service').textContent = "Услуга с това име вече съществува! Моля опитайте с друго име."
+      return;
+    }
     const serviceDescription = document.getElementById('serviceDescription').value;
     const serviceImage = document.getElementById('serviceImage').files[0];
 
@@ -96,6 +99,59 @@ document.getElementById('service-add').addEventListener('click', async (event) =
     }
 });
 
+document.getElementById('service-edit').addEventListener('click', async (event) => {
+    event.preventDefault();
+    
+    const serviceId = document.getElementById('serviceId').value;
+    const serviceName = document.getElementById('serviceName').value;
+    const serviceDescription = document.getElementById('serviceDescription').value;
+    const serviceImage = document.getElementById('serviceImage').files[0];
+
+    console.log(serviceImage.path)
+
+    if (!serviceId) {
+        document.getElementById('result-service').textContent = "Моля първо изберете услуга!";
+        return;
+    }
+
+    const formData = new FormData();
+    formData.append('name', serviceName);
+    formData.append('desc', serviceDescription);
+    formData.append('srcImage', serviceImage);
+
+
+    try {
+        const response = await fetch(`http://localhost:3001/api/services/${serviceId}`, {
+            method: 'PUT',
+            body: formData
+        });
+
+        if (!response.ok) {
+            throw new Error('Failed to update service');
+        }
+
+        displayAllServices();
+        document.getElementById('service-from').reset();
+    } catch (error) {
+        console.error('Error updating service:', error);
+    }
+});
+
+
+async function checkServiceExistence(serviceName) {
+    try {
+        const response = await fetch('http://localhost:3001/api/services');
+        if (!response.ok) {
+            throw new Error('Failed to fetch services');
+        }
+        const services = await response.json();
+        const existingService = services.find(service => service.name === serviceName);
+        return !!existingService;
+    } catch (error) {
+        console.error('Error:', error.message);
+        throw error;
+    }
+}
 
 
 document.addEventListener('DOMContentLoaded', displayAllServices());
